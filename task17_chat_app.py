3636b4a0966fd28b9b3a640096cb15c10584a53f
root <root@localhost.localdomain>
2021-03-14 14:29:25 +0530

    chatapp

---
+++ task17_chat_app.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import socket
+import os
+import threading
+from pyfiglet import Figlet
+
+s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+
+IP_address = input("Enter Your IP:")
+port = 5490
+receiver_ip=input('Receiver IP is:-')
+receiver_port=int(input('Receiver Port No.:- '))
+t=Figlet('puffy')
+print(t.renderText("\t\t CHAT APP !"))
+
+
+s.bind((IP_address , port))
+
+def receiver():
+    while True:
+        x = s.recvfrom(1024)
+        if x[0].decode()=='exit' or x[0].decode()=='bye':
+            print('\t\t\t\t\t Bye-Bye\n\n\n')
+            s.sendto('exit'.encode(), (receiver_ip,receiver_port))
+            os._exit(1)
+        clientip = x[1][0]
+        data = x[0].decode()
+        print("\n\t" + clientip + ":" + data)
+
+
+def sender():
+    while True:
+        s = socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+        print()
+        x = input("You: ")
+        s.sendto(x.encode(),(receiver_ip,receiver_port))
+        if(x=="bye" or x=="exit"):
+                os._exit(1)
+
+
+x1 = threading.Thread( target = receiver)
+x2 = threading.Thread( target = sender)
+
+x1.start()
+x2.start()
+
